ee521f417987400d9e75b6fa9b975005120b165e
boon chi <hum.boon.chi@gmail.com>
2021-04-01 16:35:57 +0800

    hide secret key

--- backend/backendMyPortfolio/settings.py
+++ backend/backendMyPortfolio/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+from dotenv import load_dotenv
+load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -18,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = ')lk!u$3$8idxp2dqjtmm-$^ig(ue=s^!d#n-c5nocws@_in+t+'
+SECRET_KEY = str(os.getenv('SECRET_KEY'))
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
